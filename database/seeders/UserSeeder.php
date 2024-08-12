0118e74ab99a94eb3a50a5af97b46bd1d92fd9c8
shandst <shandstas@gmail.com>
2024-08-12 16:54:56 +0300

    fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@filamentphp.com',
+            'password' => '$2y$10$4PHCQ7UvyUE/1zVblSmJg.BKCKel.bmUY0QTvXJu3OEez5r6.6oDG', // Password: Test7894*
+        ]);
+
         User::factory()->count(10)->create();
     }
 }
